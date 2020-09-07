0a48484112a986afd0b0d1b3fb57a16cc7bb6c54
yspaulbae <yspaulbae@gmail.com>
2020-09-07 13:07:24 +0900

    add folder '검색어별_트렌드_20200907'

---
+++ paul_branch/검색어별_트렌드_20200907/cycle_datavisual_barplot.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from pandas._libs import index
+
+tp = pd.read_csv('./sup_branch/cycle_trend.csv')
+df = pd.DataFrame(tp)
+
+x = []
+for i in range(len(df)):
+    if i%7 == 0:
+        x.append((df['date'][i]))
+
+y = []
+for i in range(len(df)):
+    if i%7 == 0:
+        y.append((df['traffic'][i]))
+print(x)
+print(y)
+
+plt.title("cycle_traffic_trend", fontweight='bold', fontsize = 12)
+plt.xlabel("date", fontweight='bold', fontsize = 12)
+plt.xticks(rotation=90)
+plt.ylabel("traffic", fontweight='bold', fontsize = 12)
+
+plt.bar(x, y, color='lightblue')
+plt.show()
+
