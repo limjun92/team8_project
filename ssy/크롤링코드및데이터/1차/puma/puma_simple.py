6134f5fe857fb0a7dbe3e4d1d98e828a6a587d19
unknown <ssy01191@gmail.com>
2020-10-01 18:23:45 +0900

    크롤링정리

--- ssy/크롤링코드및데이터/1차/puma/puma_simple.py
+++ ssy/크롤링코드및데이터/1차/puma/puma_simple.py
@@ -1,60 +1,51 @@
 import requests
 import csv
 from bs4 import BeautifulSoup
-import pandas as p
+import pandas as pd
 import os
 
 
-def get_href(ctgr, gender, type_):
-    print(f'-------{ctgr}-------')
-    page = 1
-    while True:
-        params = (
-            ('category', ctgr),
-            ('p', page),
-        )
+def get_href(ctgr_list, gender, type_):
+    df = pd.DataFrame()
+    for ctgr in ctgr_list:
+        print(f'-------{ctgr}-------')
+        page = 1
+        while True:
+            params = (
+                ('category', ctgr),
+                ('p', page),
+            )
 
-        res = requests.get('https://kr.puma.com/aramenu/ajax/productview/',  params=params, )
+            res = requests.get('https://kr.puma.com/aramenu/ajax/productview/',  params=params, )
 
-        soup = BeautifulSoup(res.content, 'html.parser', from_encoding='utf-8')
+            soup = BeautifulSoup(res.content, 'html.parser', from_encoding='utf-8')
 
-        ul = soup.select('ul.products-grid > li.item > div.puma_prod_top > a')
-        print(page, len(ul))
-        
-        for li in ul:
-            detail_href = li['href']
-            image = 'http:'+li.select_one('img')['src'].replace('210x','420x')
+            ul = soup.select('ul.products-grid > li.item ')
+            print(page, len(ul))
             
-            detail = {
-                'url' : detail_href,
-                'gender' : gender,
-                'type' : type_,
-                'images' : image,
-            }
+            for li in ul:
+                detail_href = li.select_one('a')['href']
+                image = 'http:'+li.select_one('a > img')['src'].replace('210x','420x')
+                # print(detail_href, image)
+                try:
+                    price = li.select_one('div.puma_prod_info > span').text.strip()
+                except :
+                    price = li.select_one('div.price > span').text
+                price = ''.join(price.split('원')[0].split(','))
+                # print(price)
+                detail = [['puma', gender,  type_, detail_href, price, image]]
+                df = df.append(detail)
 
-            with open(f'./puma_{gender}_{type_}_href_images.csv', 'a') as csvfile:
-                fieldnames = detail.keys()
-                csvwriter = csv.DictWriter(csvfile, fieldnames=fieldnames)
-                csvwriter.writerow(detail)
+            if len(ul)<16:
+                break
+            page+=1
 
-        if len(ul)<16:
-            break
-        page+=1
+    df.to_csv(f'./puma_{gender}_{type_}.csv')
 
-ctgr_list = [392, 161, 238, 44, 42,46,545,45,160, 391, 20, 18,22,21]
-    
-for ctgr in ctgr_list:
-    if ctgr in [392, 161, 238, 44, 42]:
-        gender = 'W'
-        type_ = 'TOP'
-    elif ctgr in [46,545,45]  :
-        gender = 'W'
-        type_ = 'BOTTOM'   
-    elif ctgr in [160, 391, 20, 18]:
-        gender = 'M'
-        type_ = 'TOP'
-    elif ctgr in [22,21]:
-        gender = 'M'
-        type_ = 'BOTTOM'
 
-    get_href(ctgr, gender, type_)
+
+
+get_href([392, 161, 238, 44, 42], 'woman', 'top')
+get_href([46,545,45], 'woman', 'bottom')
+get_href([160, 391, 20, 18], 'man', 'top')
+get_href([22,21], 'man', 'bottom')
